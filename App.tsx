7530135f9efbc182b4927dc0df926b74c9357a30
pedrohenriquebraga <gamesantos7@gmail.com>
2021-04-01 01:00:29 -0300

    Refactore code

--- App.tsx
+++ App.tsx
@@ -1,17 +1,16 @@
 import admob, {
-    firebase,
-    MaxAdContentRating,
+  MaxAdContentRating,
 } from "@react-native-firebase/admob";
 import React, { useEffect } from "react";
 import "react-native-gesture-handler";
 import AppRoutes from "./src/routes";
 
 export default function App() {
-    useEffect(() => {
-        admob().setRequestConfiguration({
-            maxAdContentRating: MaxAdContentRating.MA,
-            tagForChildDirectedTreatment: false,
-        });
-    }, []);
-    return <AppRoutes />;
+  useEffect(() => {
+    admob().setRequestConfiguration({
+      maxAdContentRating: MaxAdContentRating.MA,
+      tagForChildDirectedTreatment: false,
+    });
+  }, []);
+  return <AppRoutes />;
 }
